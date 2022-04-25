3e145de32dc34b27babebf2ead9007cfa680f1c3
Dylan Szejnblum <dylanszejnblum@gmail.com>
2022-04-25 08:51:59 -0300

    added functions

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,7 +1,11 @@
 // functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
+
+
+fn call_me() {
+    println!("I'm calling you!");
+}
 
 fn main() {
     call_me();
